368b6497fa96256c4ff8e023d553da907ef0dfea
lav <l.venier@yoctu.com>
2021-01-12 11:47:54 +0100

    add fleet

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 module.exports = {
   address: require('./structures/address').address,
   contact: require('./structures/contact').contact,
+  fleet: require('./structures/fleet').fleet,
+  vehicle: require('./structures/fleet').vehicle,
   auctionContact: require('./structures/contact').auctionContact,
   rel: require('./structures/rels').rel,
   rels: require('./structures/rels').rels,
