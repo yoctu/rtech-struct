1d2dbc606d523d62840cc242cfe91b6f4224707a
lav <lav@yoctu.com>
2021-02-17 18:58:33 +0100

    add optional timestamp to rels

--- structures/rels.js
+++ structures/rels.js
@@ -3,15 +3,23 @@ const Url = s.define('Url', require('is-url'))
 const Instance = process.env.NODE_APP_INSTANCE || 'DEMO'
 const Domain = process.env.DNSDOMAIN || '.local'
 
+const toZoulouDate = value => {
+  return value.slice(-1) !== 'Z' ? value + 'Z' : value
+}
+
+const ZoulouDate = require('./lib').zouloudate(s)
+
 const Rel = s.defaulted(
   s.object({
     key: s.size(s.string(), 2, 32),
     name: s.optional(s.size(s.string(), 2, 64)),
     url: s.optional(Url),
+    timestamp: s.optional(ZoulouDate),
     autoinvite: s.optional(s.boolean())
   }), {
   name: Instance,
-  autoinvite: false
+  autoinvite: false,
+  timestamp: new Date().toISOString()
 })
 
 const Rels = s.array(Rel)
