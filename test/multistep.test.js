8332dbbbcdacf1bb27e3ff6f3111797bfdbbab2d
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2021-03-29 10:24:18 +0200

    add test config

--- test/multistep.test.js
+++ test/multistep.test.js
@@ -21,6 +21,28 @@ describe('Dimension structure', () => {
     ], multistep())).toBeTruthy()
   })
 
+  test('Success: Mulstistep structure with 2 products but config for one', () => {
+    expect(s.is([
+      "2", "100", "100", "100", "10", "", "no", "", "", "0",
+      "EG2103CF36-A", "35 RUE DE CALAIS", "67100", "STRASBOURG", "France", "FR", "48.5252,7.7824", "Europe/Paris", "test1", "test1", "test1@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "EG2103CF36-B", "14 RUE GORGE DE LOUP", "69009", "LYON", "France", "FR", "45.77,4.8041", "Europe/Paris", "test2", "test2", "test2@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "2", "100", "100", "100", "10", "", "no", "", "", "0",
+      "EG2103CF36-A", "35 RUE DE CALAIS", "67100", "STRASBOURG", "France", "FR", "48.5252,7.7824", "Europe/Paris", "test1", "test1", "test1@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "EG2103CF36-B", "14 RUE GORGE DE LOUP", "69009", "LYON", "France", "FR", "45.77,4.8041", "Europe/Paris", "test2", "test2", "test2@test.fr", "090809080908", "2021-03-01T01:00:00","2021-03-01T01:00:00"
+    ], multistep({shaq: {maxsteps: 1}}))).toBeFalsy()
+  })
+
+  test('Success: Mulstistep structure with 2 products and config for two', () => {
+    expect(s.is([
+      "2", "100", "100", "100", "10", "", "no", "", "", "0",
+      "EG2103CF36-A", "35 RUE DE CALAIS", "67100", "STRASBOURG", "France", "FR", "48.5252,7.7824", "Europe/Paris", "test1", "test1", "test1@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "EG2103CF36-B", "14 RUE GORGE DE LOUP", "69009", "LYON", "France", "FR", "45.77,4.8041", "Europe/Paris", "test2", "test2", "test2@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "2", "100", "100", "100", "10", "", "no", "", "", "0",
+      "EG2103CF36-A", "35 RUE DE CALAIS", "67100", "STRASBOURG", "France", "FR", "48.5252,7.7824", "Europe/Paris", "test1", "test1", "test1@test.fr", "090809080908", "2021-03-01T01:00:00", "2021-03-01T01:00:00",
+      "EG2103CF36-B", "14 RUE GORGE DE LOUP", "69009", "LYON", "France", "FR", "45.77,4.8041", "Europe/Paris", "test2", "test2", "test2@test.fr", "090809080908", "2021-03-01T01:00:00","2021-03-01T01:00:00"
+    ], multistep({shaq: {maxsteps: 2}}))).toBeTruthy()
+  })
+
   test('Failed: Mulstistep structure (missing a package field)', () => {
     expect(s.is([
       "2", "100", "100", "10", "", "no", "", "", "0",
