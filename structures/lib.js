57c6acb605bc00275e428aaade02447b601cb52a
Vincent Simonin <v.simonin@redspher.com>
2021-04-14 17:59:58 +0200

    Change the way that Timzone string are validated
    * Use Intl lib now linked with system

--- structures/lib.js
+++ structures/lib.js
@@ -1,5 +1,16 @@
+const s = require('superstruct')
+
 const zdReg = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/
 const pReg = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/
+
+exports.Tz = s.define('Tz', value => {
+  try {
+    new Intl.DateTimeFormat("en-US", { timeZone: value }).format();
+    return true;
+  } catch (e) {
+    return false;
+  }
+})
 
 exports.zouloudate = function (s) {
   return s.pattern(s.string(), zdReg)
