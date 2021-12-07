a7120bec8ea0ff10a3f94a3aa1d68bfdd135c422
Vincent Simonin <v.simonin@redspher.com>
2021-12-07 13:46:11 +0100

    Add new feature on ISO 8601 date time format

--- structures/lib.js
+++ structures/lib.js
@@ -1,7 +1,7 @@
 const s = require('superstruct')
 
 const zdReg = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/
-const isoReg = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?(Z|(\+|\-)\d{2}:\d{2})$/
+const isoReg = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?(Z|(\+|\-)\d{2}:?\d{2})$/
 const pReg = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/
 
 exports.Tz = s.define('Tz', value => {
