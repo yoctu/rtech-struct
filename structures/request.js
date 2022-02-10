a8119925c30e06154db773641b287932a79197ec
miranto <m.razafindrazaka@redspher.com>
2022-02-10 10:34:17 +0100

    Remove call_in_contact on the request structure

--- structures/request.js
+++ structures/request.js
@@ -10,7 +10,6 @@ const { OrderType } = require('./order_type')
 const { GetItNow } = require('./get_it_now')
 const { ValidityTime } = require('./validity_time')
 const { CustomerInterlocutor } = require('./customer_interlocutor')
-const { CallInContact } = require('./call_in_contact')
 
 const Request = s.object({
     key: s.optional(NoEmptyString),
@@ -29,7 +28,6 @@ const Request = s.object({
     creator: s.optional(NoEmptyString),
     target: s.optional(s.size(s.array(NoEmptyString), 0, 100)),
     comment: s.optional(s.size(s.string(), 2, 512)),
-    call_in_contact: s.optional(CallInContact),
 })
 
 module.exports = {
