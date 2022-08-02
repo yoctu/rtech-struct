e5c28f449e1bbfde0298300c26863abe85b8956f
miranto <m.razafindrazaka@redspher.com[core]>
2022-08-02 15:01:49 +0200

    additional address

--- structures/place.js
+++ structures/place.js
@@ -1,10 +1,23 @@
 const s = require('superstruct')
-const { Tz } = require('./lib')
+const {Tz} = require('./lib')
 
 exports.place = function (s) {
-  return s.tuple([s.size(s.string(), 2, 128),s.size(s.string(), 1, 32),s.size(s.string(), 2, 64),s.size(s.string(), 2, 32),s.size(s.string(), 2, 32)])
+    return s.tuple([s.size(s.string(), 2, 128), s.size(s.string(), 1, 32), s.size(s.string(), 2, 64), s.size(s.string(), 2, 32), s.size(s.string(), 2, 32)])
 }
 
 exports.placeTZ = function (s) {
-  return s.tuple([s.size(s.string(), 2, 128),s.size(s.string(), 1, 32),s.size(s.string(), 2, 64),s.size(s.string(), 2, 32),s.size(s.string(), 2, 32), s.optional(Tz)])
+    return s.tuple([s.size(s.string(), 2, 128), s.size(s.string(), 1, 32), s.size(s.string(), 2, 64), s.size(s.string(), 2, 32), s.size(s.string(), 2, 32), s.optional(Tz)])
+}
+
+exports.placeTZ2 = function (s) {
+    return s.tuple(
+        [
+            s.size(s.string(), 2, 128),
+            s.size(s.string(), 1, 32),
+            s.size(s.string(), 2, 64),
+            s.size(s.string(), 2, 32),
+            s.size(s.string(), 2, 32),
+            s.optional(Tz),
+            s.optional(s.size(s.string(), 2, 128))
+        ])
 }
