287298c27eeaf16e21b30a98aa94a3340327440d
miranto <m.razafindrazaka@redspher.com[core]>
2022-08-04 13:54:59 +0200

    fix indent

--- structures/multistep.js
+++ structures/multistep.js
@@ -1,28 +1,30 @@
 const s = require('superstruct')
 
 const Multistep = function (config = null) {
-  let MaxSteps = 50
-  if (config && config.shaq && config.shaq.maxsteps) MaxSteps = config.shaq.maxsteps
+    let MaxSteps = 50
+    if (config && config.shaq && config.shaq.maxsteps) MaxSteps = config.shaq.maxsteps
 
-  return s.size(s.array(s.string()), 38, 38 * MaxSteps)
+    return s.size(s.array(s.string()), 38, 38 * MaxSteps)
 }
 
 const PackageV1 = s.refine(
-    s.array(s.size(s.string(), 0,64)),
+    s.array(s.size(s.string(), 0, 64)),
     'PackageV1',
     (value) => {
         return value.length >= 6 && (value.length % 6 === 0)
-    })
+    }
+)
 
 const PackageV2 = s.refine(
-    s.array(s.size(s.string(), 0,64)),
+    s.array(s.size(s.string(), 0, 64)),
     'PackageV2',
     (value) => {
-      return value.length >= 9 && (value.length % 9 === 0)
-    })
+        return value.length >= 9 && (value.length % 9 === 0)
+    }
+)
 
 module.exports = {
-  packageV1: PackageV1,
-  packageV2: PackageV2,
-  multistep: Multistep
+    packageV1: PackageV1,
+    packageV2: PackageV2,
+    multistep: Multistep
 }
