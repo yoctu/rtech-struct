491c2a98c5b1589ccaf38a6af79a3b3ef738d64c
mafricalucas <lucasmafrica57@gmail.com>
2022-09-08 13:14:32 +0200

    Create MarketplaceSpot and VirtualOrder order types into Request structure

--- structures/order_type.js
+++ structures/order_type.js
@@ -1,7 +1,8 @@
 const s = require('superstruct')
 
 const OrderType = s.object({
-    type: s.enums(['getitnow'])
+    type: s.enums(['getitnow', 'marketplace_spot', 'virtual_order']),
+    delegated_target: s.optional(s.array(s.string()))
 })
 
 module.exports = {
