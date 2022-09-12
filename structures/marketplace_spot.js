3974186e092f62686450ab4be01bd155489d3311
mafricalucas <lucasmafrica57@gmail.com>
2022-09-12 14:35:18 +0200

    add size to delegated_target array

--- structures/marketplace_spot.js
+++ structures/marketplace_spot.js
@@ -1,8 +1,9 @@
 const s = require('superstruct')
+const { NoEmptyString } = require('./string')
 
 const MarketplaceSpot = s.object({
     type: s.literal('marketplace_spot'),
-    delegated_target: s.array(s.string())
+    delegated_target: s.size(s.array(NoEmptyString), 1),
 })
 
 module.exports = {
