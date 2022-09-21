bf27c8f5d446dd871a38a11742914b695116c245
GBo <g.boulangeot@redspher.com>
2022-09-21 14:16:26 +0200

    add def and min/max values

--- structures/notification/acknowledge.js
+++ structures/notification/acknowledge.js
@@ -2,9 +2,12 @@ const s = require('superstruct')
 const isUuid = require('is-uuid')
 const uuidStruct = s.define('uuid', isUuid.v4)
 
+const NOTIFICATION_IDS_SIZE_MIN = 1;
+const NOTIFICATION_IDS_SIZE_MAX = 512;
+
 const structure = s.object({
-    notificationsIds: s.size(s.array(uuidStruct), 1),
-    name: s.string()
+    name: s.string(),
+    notificationsIds: s.size(s.array(uuidStruct), NOTIFICATION_IDS_SIZE_MIN, NOTIFICATION_IDS_SIZE_MAX)
 })
 
-module.exports = { structure }
+module.exports = { structure, NOTIFICATION_IDS_SIZE_MIN, NOTIFICATION_IDS_SIZE_MAX }
