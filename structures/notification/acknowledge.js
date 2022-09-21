c437e10c5bcdf1776e722cca68ad54436e5b19c3
GBo <g.boulangeot@redspher.com>
2022-09-21 14:05:25 +0200

    s.type removed

--- structures/notification/acknowledge.js
+++ structures/notification/acknowledge.js
@@ -2,10 +2,9 @@ const s = require('superstruct')
 const isUuid = require('is-uuid')
 const uuidStruct = s.define('uuid', isUuid.v4)
 
-const structure = s.object(
-    s.type({
-      notificationsIds: s.size(s.array(uuidStruct), 1),
-      name: s.string(),
-    }))
+const structure = s.object({
+    notificationsIds: s.size(s.array(uuidStruct), 1),
+    name: s.string()
+})
 
 module.exports = { structure }
